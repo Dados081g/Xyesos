2c9c7153beb19af6b1c12d99e0c5d71fc34880ca
Dados081g <egorevseev062@gmail.com>
2026-09-19 01:41:55 +0300

    Update shop_bot-3-5.py

--- shop_bot-3-5.py
+++ shop_bot-3-5.py
@@ -18,7 +18,7 @@ from aiogram.types import (
 
 # ======================= НАСТРОЙКИ =======================
 
-BOT_TOKEN = "ВАШ_ТОКЕН_ЗДЕСЬ"          # токен от @BotFather
+BOT_TOKEN = "8342142309:AAHnGHfqM5M0_PI1WjJkfscvRk-yZemM8Ic"          # токен от @BotFather
 ADMIN_ID = 123456789                   # ваш Telegram ID (узнать можно у @userinfobot)
 ADMIN_PASSWORD = "maksumtop1"          # пароль для входа в /admin
 CARD_NUMBER = "2202 2083 0536 9622"
